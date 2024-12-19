50e3afbf48fa1e3230e3c5f50f68cc641db81e62
Richad <81878873+RichardGou@users.noreply.github.com>
2024-12-19 12:36:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def main():
         trainloader, validloader, testloader = get_synthetic_data(output_length, batch_size=batch_size)
 
     elif args.data == "traffic":
-        output_length = 36
+        output_length = 24
         print("OUTPUT LENGTH TRAFFIC : ",output_length)
         path_data = args.path_data or "data/traffic/traffic.txt"
         batch_size=32
